a6304ee2b67e66d9d329e202796a8d5c2d0d8b73
fongxuan <fong1143@gmail.com>
2017-12-01 11:31:07 +0800

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -47,9 +47,29 @@ app.get('/todos/:id', (req, res) => {
 		//res.send({todo: todo});
 		res.send({todo});
 
-	}, (e) => {
+	}).catch((e) => {
 		res.status(400).send();
 	});
+});
+
+app.delete('/todos/:id', (req,res) => {
+	//get the id
+	var id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if(!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+
+	}).catch((e) => {
+		res.status(400).send();
+	});
+	
 });
 
 app.listen(port, () => {
