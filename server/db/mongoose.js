02e6d9e70783aa1397d1025a86bf158298b7489d
fongxuan <fong1143@gmail.com>
2017-12-01 14:11:20 +0800

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,14 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI );
 
 
 //module.exports = { mongoose: mongoose };
 module.exports = { mongoose };
 //everything starts util mongoose connect to db,
 //you don't have to worry about the order
+
 
 
