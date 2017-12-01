e4222891f27d1151d1bcc09550863f2677d0e579
fongxuan <fong1143@gmail.com>
2017-12-01 21:46:10 +0800

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -43,7 +43,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
 	//we want to use 'this' keyword,
 	//but arrow func don't bind the keyword.
-	var user = this;
+	var user = this;		//indivisual method
 	var access = 'auth';
 	var token  = jwt.sign({_id: user._id.toHexString(), access}, 'secret').toString();
 
@@ -51,6 +51,25 @@ UserSchema.methods.generateAuthToken = function () {
 	
 	return user.save().then(() => {
 		return token;
+	});
+};
+
+UserSchema.statics.findByToken = function (token) {
+	var User = this;		//model method
+	var decoded;
+
+	try{
+		decoded = jwt.verify(token, 'secret');
+	}catch (e) {
+		return Promise.reject('test');			//Promise for chaining
+		/*return new Promise((resolve, reject) => {
+			reject();
+		});*/
+	}
+	return User.findOne({				//return that in order to chaining, that we can call then, quote is needed for dot
+		_id: decoded._id,
+		'tokens.token': token,
+		'tokens.access': 'auth'
 	});
 };
 
