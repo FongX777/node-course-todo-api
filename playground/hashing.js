291531c9415e460f1fa910e6065f11a2d4460fd2
fongxuan <fong1143@gmail.com>
2017-12-02 11:31:21 +0800

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,26 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '123abc';
+
+//async
+bcrypt.genSalt(10, (err, salt) => {
+	bcrypt.hash(password, salt, (err, hash) => {
+		console.log(hash);
+	});
+});
+
+
+var hashedPassword = '$2a$10$nfqBMFbPBhBJSyUsenecYea9DJ0N/IEJpJkbj7RvVzlZVjqnBNyXO';
+//async
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	console.log(res);
+});
+
+
+
+/*
 var data = {
 	id: 10
 };
